4205994663dfb453d13835c88a6827ba2ff75928
IluaSN <ilua.snegirev.97@mail.ru>
2021-11-18 23:51:10 +1000

    Work.....

--- test/test.cpp
+++ test/test.cpp
@@ -5,8 +5,37 @@ using namespace std;
 
 int main()
 {
-    
-    cout << "Hello World!\n";
+    setlocale(LC_ALL,"ru");
+
+	struct Game
+	{
+		char cArr[3][3];
+		char X = 'X', O = 'O', NOPE = '_';
+		string stName = "PL", stName2 = "PC";
+
+
+	};
+	Game aaa;
+	cout << "Выберите символ для игры: " << endl
+		 << "X или O" << endl;
+	char a, *cPL, *cPC;
+	cin >> a;
+	
+	if (a == 'x')
+	{
+		cPL = &aaa.X;
+		cPC = &aaa.O;
+	}
+	else
+	{
+		cPL = &aaa.O;
+		cPC = &aaa.X;
+	}
+	cout << "Вы выбрали: " << *cPL << endl;
+	cout << "ПК выбрал: " << *cPC << endl;
+
+
+
 }
 
 
