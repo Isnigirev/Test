c478035cecdf714b2c54d51bc0703f8e509114b2
IluaSN <ilua.snegirev.97@mail.ru>
2021-11-19 22:54:13 +1000

    Work......

--- test/test.cpp
+++ test/test.cpp
@@ -1,27 +1,51 @@
 ﻿#include <iostream>
+#include <ctime>
 using namespace std;
 
+int *flag1, *flag2;
 
+struct st
+{	
+	char X = 'X', O = 'O', NOPE = '_';
+	char cArr[3][3]{};
+	string stName = "PL", stName2 = "PC";
+};
+
+int random (int i1, int i2)
+{
+	st aaa;
+	srand(time(nullptr));
+	if (aaa.cArr[i1][i2] == 0 )
+	{
+	cout << "weee: " << endl;
+	
+	int i1 = rand() % 3, i2 = rand() % 3;
+
+	flag1 = &i1; flag2 = &i2;
+	}
+	else
+	{
+	
+		int i1 = rand() % 3, i2 = rand() % 3;
+
+
+
+
+	}
+return 0;
+}
 
 int main()
 {
+	
     setlocale(LC_ALL,"ru");
-
-	struct Game
-	{
-		char cArr[3][3];
-		char X = 'X', O = 'O', NOPE = '_';
-		string stName = "PL", stName2 = "PC";
-
-
-	};
-	Game aaa;
+	st aaa;
 	cout << "Выберите символ для игры: " << endl
 		 << "X или O" << endl;
 	char a, *cPL, *cPC;
 	cin >> a;
 	
-	if (a == 'x')
+	if (a == 'x'|| a == 'X')
 	{
 		cPL = &aaa.X;
 		cPC = &aaa.O;
@@ -33,9 +57,13 @@ int main()
 	}
 	cout << "Вы выбрали: " << *cPL << endl;
 	cout << "ПК выбрал: " << *cPC << endl;
+	
+	srand(time(nullptr));
+	
+	int i1 = rand() % 3, i2 = rand() % 3;
 
+	random (i1, i2);
 
-
+	cout << *flag1 << *flag2 << endl;
 }
-
 
