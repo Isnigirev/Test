c76e69cd60ecc2627826201dfa248ab608c5158f
IluaSN <ilua.snegirev.97@mail.ru>
2021-12-12 21:10:25 +1000

    Return 0

--- test/test.cpp
+++ test/test.cpp
@@ -1,73 +1,13 @@
 ﻿#include <iostream>
-#include <ctime>
+
 using namespace std;
 
-int *flag1, *flag2;
-
-struct st
-{	
-	char X = 'X', O = 'O', NOPE = '_';
-	char cArr[3][3]{};
-	string stName = "PL", stName2 = "PC";
-};
-
-int random (int i1, int i2)
-{
-	st aaa;
-
-	srand(time(nullptr));
-
-	if (aaa.cArr[i1][i2] == 0 )
-	{	
-	int i1 = rand() % 3, i2 = rand() % 3;
-
-	cout << i1 <<"______" << i2 << endl;
-
-	flag1 = &i1; flag2 = &i2;
-	}
-	else
-	{
-	
-		int i1 = rand() % 3, i2 = rand() % 3;
-
-
-
-
-	}
-return 0;
-}
 
 int main()
 {
-	
     setlocale(LC_ALL,"ru");
-	st aaa;
-	cout << "Выберите символ для игры: " << endl
-		 << "X или O" << endl;
-	char a, *cPL, *cPC;
-	cin >> a;
 	
-	if (a == 'x'|| a == 'X')
-	{
-		cPL = &aaa.X;
-		cPC = &aaa.O;
-	}
-	else
-	{
-		cPL = &aaa.O;
-		cPC = &aaa.X;
-	}
-	cout << "Вы выбрали: " << *cPL << endl;
-	cout << "ПК выбрал: " << *cPC << endl;
 	
-	srand(time(nullptr));
-	
-	int i1 = rand() % 3, i2 = rand() % 3;
-
-	cout << i1 << "______" << i2 << endl << endl;
-
-	random(i1, i2);
-
 	return 0;
 }
 
