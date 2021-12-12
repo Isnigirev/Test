370dbc7edfa4098c231edcd9fa25d67149bf2e8f
isnegirev34@gmail.com <isnegirev34@gmail.com>
2021-12-13 08:20:50 +1000

    1

--- test/test.cpp
+++ test/test.cpp
@@ -1,12 +1,19 @@
 ﻿#include <iostream>
 
 using namespace std;
+//Шаблоны
+template <class T1, class T2>
+T1 AW(T1 one, T2 two)
+{
+	return one + two;
+}
 
 
 int main()
 {
     setlocale(LC_ALL,"ru");
 	
+	cout << AW(2.4,3) <<endl;
 	
 	return 0;
 }
